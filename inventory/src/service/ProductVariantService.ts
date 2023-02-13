99b944b9944f7217678075b4de3e7a15e93cd69e
doanhthuc <thuc.pham-doanh@ubisoft.com>
2023-02-13 15:14:14 +0700

    fix bug update product

--- inventory/src/service/ProductVariantService.ts
+++ inventory/src/service/ProductVariantService.ts
@@ -401,6 +401,7 @@ export class ProductVariantService implements IService {
 
     async serveUpdateVariantRPCRequest(request: UpdateVariantRPCRequest) {
         const { productId, productVariantList, colorNameList, sizeNameList } = request;
+        console.log("🚀 ~ file: ProductVariantService.ts:404 ~ ProductVariantService ~ serveUpdateVariantRPCRequest ~ productVariantList", productVariantList)
         try {
             await this.productVariantRepository.deleteProductVariantByProductId(productId);
         } catch (error) {
